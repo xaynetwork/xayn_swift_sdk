7258ae56418f48ad8a7419332ad13604bd395fdf
Peter Stojanowski <peter.stojanowski.external@xayn.com>
2022-10-31 09:06:29 +0100

    Add a CLI example project

---
+++ example/CLI/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CLI",
+    platforms: [.macOS(.v12)],
+    products: [
+        .executable(name: "xayn", targets: ["CLI"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.4"),
+        .package(url: "https://github.com/xaynetwork/xayn_swift_sdk", from: "1.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "CLI",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "XaynSDK", package: "xayn_swift_sdk"),
+            ]),
+    ]
+)
---
+++ example/CLI/Sources/CLI/CLI.swift
@@ -0,0 +1,62 @@
+import ArgumentParser
+import Foundation
+import XaynSDK
+
+enum XaynSDKFunction: String, ExpressibleByArgument {
+    case personalization
+    case interactions
+    case documents
+}
+
+@main
+struct Repeat: ParsableCommand, AsyncParsableCommand {
+    @Argument(help: "The function to run (personalization, interactions, documents).")
+    var function: XaynSDKFunction
+
+    mutating func run() async throws {
+        switch function {
+        case .personalization:
+            await personalization()
+        case .interactions:
+            await interactions()
+        case .documents:
+            await documents()
+        }
+    }
+
+    private func personalization() async {
+        print("Calling /users/{user_id}/personalized_documents ...")
+        let client = XaynClient(userId: UUID())
+        do {
+            let response = try await client.personalizedDocuments()
+            print("Received response: \(response)")
+        } catch {
+            print("Received error: \(error)")
+        }
+    }
+    
+    private func interactions() async {
+        print("Calling /users/{user_id}/interactions ...")
+        let client = XaynClient(userId: UUID())
+        do {
+            try await client.likeDocument(documentId: "doc 1")
+            print("Success")
+        } catch {
+            print("Received error: \(error)")
+        }
+    }
+    
+    private func documents() async {
+        print("Calling /documents ...")
+        let client = XaynClient(userId: UUID())
+        let doc = IngestedDocument(id: "document id 0001",
+                                   snippet: "Snippet of text that will be used to calculate embeddings.",
+                                   properties: ["title": "Document title"])
+        do {
+            try await client.addDocuments([doc])
+            print("Success")
+        } catch {
+            print("Received error: \(error)")
+        }
+    }
+}

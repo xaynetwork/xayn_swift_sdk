9716d2a323d7329afd79ea100b92c09ed7b69a5b
Simon Joecks <simon.joecks@gmail.com>
2022-11-10 11:01:20 -0500

    enforce an API key in the CLI example

--- example/CLI/Sources/CLI/CLI.swift
+++ example/CLI/Sources/CLI/CLI.swift
@@ -8,25 +8,36 @@ enum XaynSDKFunction: String, ExpressibleByArgument {
     case documents
 }
 
+enum CliError: Error {
+    case missingApiKeit(String)
+}
+
 @main
 struct MainCommand: ParsableCommand, AsyncParsableCommand {
     @Argument(help: "The function to run (personalization, interactions, documents).")
     var function: XaynSDKFunction
 
+    
+
     mutating func run() async throws {
+        let apiKey = ProcessInfo.processInfo.environment["API_KEY"]
+        if (apiKey == nil)  {
+            throw CliError.missingApiKeit("Missing API_KEY env")
+        }
+
         switch function {
         case .personalization:
-            await personalization()
+            await personalization(apiKey: apiKey!)
         case .interactions:
-            await interactions()
+            await interactions(apiKey: apiKey!)
         case .documents:
-            await documents()
+            await documents(apiKey: apiKey!)
         }
     }
 
-    private func personalization() async {
+    private func personalization(apiKey : String) async {
         print("Calling /users/{user_id}/personalized_documents ...")
-        let client = XaynClient(apiKey: "<your API key>", userId: "r")
+        let client = XaynClient(apiKey: apiKey, userId: "r")
         do {
             let response = try await client.personalizedDocuments(count: 5)
             print("Received documents: \(response.documents)")
@@ -35,9 +46,9 @@ struct MainCommand: ParsableCommand, AsyncParsableCommand {
         }
     }
     
-    private func interactions() async {
+    private func interactions(apiKey : String) async {
         print("Calling /users/{user_id}/interactions ...")
-        let client = XaynClient(apiKey: "<your API key>", userId: "r")
+        let client = XaynClient(apiKey: apiKey, userId: "r")
         do {
             try await client.likeDocument(documentId: "document id 0001")
             print("Success")
@@ -46,9 +57,9 @@ struct MainCommand: ParsableCommand, AsyncParsableCommand {
         }
     }
     
-    private func documents() async {
+    private func documents(apiKey : String) async {
         print("Calling /documents ...")
-        let client = XaynClient(apiKey: "<your API key>", userId: "r")
+        let client = XaynClient(apiKey:apiKey, userId: "r")
         let doc = IngestedDocument(id: "document id 0001",
                                    snippet: "Snippet of text that will be used to calculate embeddings.",
                                    properties: ["title": "Document title"])

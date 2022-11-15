c9318f9aa25c903e7c24f04e1c8e258fd1052496
Simon Joecks <simon.joecks@gmail.com>
2022-11-15 10:47:35 -0500

    Change SDK path back to absolute URL

--- example/CLI/Package.swift
+++ example/CLI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.4"),
-        .package(url: "../../", from: "1.0.9")
+        .package(url: "https://github.com/xaynetwork/xayn_swift_sdk", from: "1.0.10")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
